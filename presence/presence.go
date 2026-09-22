a5f78d702e6a3fa63927c79b48730870cb2f338b
Zhengqun Koo <zhengqun.koo@gmail.com>
2026-09-22 13:35:26 +0800

    Use exported function to add file:// URI scheme to path

--- presence/presence.go
+++ presence/presence.go
@@ -7,9 +7,7 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/url"
 	"path/filepath"
-	"strings"
 	"sync"
 	"time"
 
@@ -90,12 +88,7 @@ func (p *Presence) SetActive(uri workspaceapi.URI) {
 		log.Printf("set activity: error expanding path: %v", err)
 		return
 	}
-	u, err := url.Parse(absPath)
-	if err != nil {
-		log.Printf("set activity: url parse: %v", err)
-		return
-	}
-	absPathURI, err := makeFileURI(u)
+	absPathURI, err := workspaceapi.CurrentUserHostURI(absPath)
 	if err != nil {
 		log.Printf("set activity: error parsing path: %v", err)
 		return
@@ -110,33 +103,6 @@ func (p *Presence) SetActive(uri workspaceapi.URI) {
 	p.activePath = relPath
 	p.hasActive = true
 	p.scheduleUpdate()
-}
-
-func makeFileURI(u *url.URL) (URI, error) {
-	path := sanitizeFilePath(u.Path)
-	u.Path = path
-	if u.Scheme == "" {
-		return workspaceapi.URI{}, fmt.Errorf("invalid empty scheme %#v", u)
-	}
-	return workspaceapi.URI{uri: u.String(), parsed: *u, name: filepath.Base(path)}, nil
-}
-
-func sanitizeFilePath(resource string) string {
-	resource = filepath.Clean(resource)
-	return sanitizeLine(resource)
-}
-
-func sanitizeLine(in string) string {
-	var b strings.Builder
-	for _, r := range in {
-		switch r {
-		case '\x00':
-		case '\n':
-		default:
-			b.WriteRune(r)
-		}
-	}
-	return b.String()
 }
 
 // Close clears presence when the extension shuts down.
